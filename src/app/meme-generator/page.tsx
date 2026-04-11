043c8423787a224c966ae3c7d1e825850726a50d
Bruno Sertić <sertic.bruno@gmail.com>
2026-04-11 13:51:06 +0200

    s

--- src/app/meme-generator/page.tsx
+++ src/app/meme-generator/page.tsx
@@ -2,7 +2,7 @@ import { AdvancedMemeGenerator } from '@/components/meme/AdvancedMemeGenerator';
 
 export default function AdvancedMemeGeneratorPage() {
   return (
-    <main className="bg-[#f7f4ee] dark:bg-gray-950 py-8">
+    <main className="bg-[#f7f4ee] dark:bg-gray-950 py-2 md:py-4">
       <section className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="p-0">
           <AdvancedMemeGenerator />
